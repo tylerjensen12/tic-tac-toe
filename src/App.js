07d57aaefbeb272e0c80e4d8aa3de9a9d082719a
Tyler Jensen <troubletimestwo97@gmail.com>
2022-11-28 17:25:52 -0700

    added handleClick and winner

--- src/App.js
+++ src/App.js
@@ -3,18 +3,53 @@ import Square from "./Square";
 import React, { useState } from "react";
 
 function App() {
-  // let myVar = "This is a prop";
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
 
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""]);
+    setPlayer(true);
+  };
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return `${squares[a]} won!`;
+      }
+    }
+    return "Who will win?";
+  }
+
   return (
-    <div>
-      <Square
-        squares={squares}
-        setSquares={setSquares}
-        player={player}
-        setPlayer={setPlayer}
-      />
+    <div className="App">
+        <span>{calculateWinner(squares)}</span>
+      <div className="container">
+        {squares.map((val, index) => {
+          return (
+            <Square
+            key={index}
+            index={index}
+            squareValue={val}
+            squares={squares}
+            setSquares={setSquares}
+            player={player}
+            setPlayer={setPlayer}
+            />
+            );
+          })}
+      </div>
+          <button onClick={handleClick}>Reset</button>
     </div>
   );
 }
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,9 +1,27 @@
-// import React from "react";
+import React from "react";
 
 const Square = (props) => {
-  console.log(props.squares, props.player);
-  
-  return <div className="square"></div>;
+  const handleClick = () => {
+    if (!props.squareValue) {
+      if (props.player) {
+        props.squares.splice(props.index, 1, "X");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      } else props.squares.splice(props.index, 1, "O");
+      props.setSquares(props.squares);
+      props.setPlayer(!props.player);
+    }
+  };
+
+  return (
+    <div className="square" onClick={handleClick}>
+      {props.squareValue === "O" ? (
+        <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" alt="DevMountain Logo"/>
+      ) : (
+        props.squareValue
+      )}
+    </div>
+  );
 };
 
 export default Square;
